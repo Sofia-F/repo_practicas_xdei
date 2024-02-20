1b75a4e660085ab25a295ddb40e4ccd7dc3d066f
Sofía Flores <sofia.flores@udc.es>
2024-02-20 13:58:05 +0100

    Update ngsiv2.py

--- ngsiv2.py
+++ ngsiv2.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-# Fuction to create a new entity.
+# POST - create_entity - Fuction to create a new entity.
 def create_entity(entity):
     url = "http://localhost:1026/v2/entities/"
 
@@ -27,6 +27,7 @@ def read_attr(id, attr):
     response = requests.request("GET", url)
     return (response.status_code, response.text)
 
+# DELETE - delete_attr(id, attr)
 def delete_attr(id, attr):
     url = "http://localhost:1026/v2/entities/" + id + "/attrs/" + attr
     response = requests.request("DELETE", url)
@@ -39,19 +40,28 @@ def update_attrs(id, attrs_vals):
     response = requests.request("PATCH", url, headers=headers, data=payload)
     return (response.status_code, response.text)
 
+# PUT - update_attr
 def update_attr(id, attr, val):
-    url = "http://localhost:1026/v2/entities/"+ id + "/attrs/" + attr
-    payload = json.dumps({"value": val})  # Ensure payload is a JSON object
-    headers = {'Content-Type': 'application/json'}
+    url = "http://localhost:1026/v2/entities/"+id+"/attrs/"+attr+"/value"
+
+    payload = val
+    headers = {'Content-Type': 'text/plain'}
     response = requests.request("PUT", url, headers=headers, data=payload)
-    return (response.status_code, response.text)
+    print(response.status_code, response.text)
 
-def list_entities(type):
-    url = "http://localhost:1026/v2/entities/?type="+type
+# GET - list_entities
+# def list_entities(type):
+#     url = "http://localhost:1026/v2/entities/?type="+type
+#     response = requests.request("GET", url)
+
+#     return (response.status_code, response.text)
+
+
+def list_entities(type = None,options = 'count', attrs = None):
+    url = "http://localhost:1026/v2/entities/"
+
     response = requests.request("GET", url)
-
-    return (response.status_code, response.text)
-
+    print(response.status_code, response.text)
 
 
 if __name__ == "__main__":
