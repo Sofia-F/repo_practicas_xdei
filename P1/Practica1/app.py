cb3145adc5e0b905dd39cb469e89f1d4064be61f
Sofía Flores <sofia.flores@udc.es>
2024-02-28 19:36:53 +0100

    .

--- P1/Practica1/app.py
+++ P1/Practica1/app.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from flask import render_template
 import ngsiv2
 from flask import Flask
-
+import math
 app = Flask(__name__)
 
 
@@ -20,22 +20,29 @@ def about():
 def stores():
  (status, stores) = ngsiv2.list_entities(type = 'Store', options = 'keyValues')
 #  print(status)
-#  pprint.pprint(products)
+#  pprint.pprint(stores)
  if status == 200:
     return render_template('stores.html', stores = stores)
  
 @app.route('/stores/<id>')
+
 def store(id):
  (status, store) = ngsiv2.read_entity(id)
  print(status)
- pprint.pprint(product)
  if status == 200:
     (status, inventory_items) = ngsiv2.list_entities(type = 'InventoryItem',
                                                     options = 'keyValues',
                                                     attrs = f'refStore=={id}')
     print(inventory_items)
     if status == 200:
-        return render_template('store.html', store = store, inventory_items = inventory_items)
+        lon_deg = store['location']['value']['coordinates'][0]
+        lat_deg = store['location']['value']['coordinates'][1]
+        zoom = 15
+        n = 1 << zoom
+        lat_rad = math.radians(lat_deg)
+        xtile = int((lon_deg + 180.0) / 360.0 * n)
+        ytile = int((1.0 - math.asinh(math.tan(lat_rad)) / math.pi) / 2.0 * n)
+        return render_template('store.html', store = store, inventory_items = inventory_items, zoom = zoom, xtile = xtile, ytile = ytile)
 
 @app.route('/products/')
 def products():
